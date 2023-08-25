6160e73073ba4ee27c8d7727d9a9a1292ef39dbd
chanho lee <leech3878@naver.com>
2023-08-25 16:44:07 +0900

    새 리스트 생성시 카테고리 선택 버튼 추가

--- TodoList/ViewController/AddViewController.swift
+++ TodoList/ViewController/AddViewController.swift
@@ -9,10 +9,13 @@ import UIKit
 
 class AddViewController: UIViewController {
     
+    var category = "공부"
+    @IBOutlet weak var categoryButton: UIButton!
     @IBOutlet weak var customLabel1: UITextField!
     @IBOutlet weak var customLable2: UITextField!
+    
     @IBAction func saveButton(_ sender: Any) {
-        let newTodo = Todo(category: "공부", content: customLabel1.text!, dueDate : customLable2.text, isComplete: false)
+        let newTodo = Todo(category: category, content: customLabel1.text!, dueDate : customLable2.text, isComplete: false)
         let section: Int
         switch newTodo.category {
         case "공부":
@@ -28,8 +31,31 @@ class AddViewController: UIViewController {
         SaveData()
         self.navigationController?.popViewController(animated: true)
     }
+    
     @objc func dateChange(_ sender : UIDatePicker) {
         customLable2.text = sender.date.toString()
+    }
+    
+    func SetupCategoryMenu () {
+        let study = UIAction(title: "공부", handler: { _ in
+            self.categoryButton.setTitle("공부", for: .normal)
+            self.category = "공부"
+        })
+        let project = UIAction(title: "프로젝트", handler: { _ in
+            self.categoryButton.setTitle("프로젝트", for: .normal)
+            self.category = "프로젝트"
+        })
+        let life = UIAction(title: "일상", handler: { _ in
+            self.categoryButton.setTitle("일상", for: .normal)
+            self.category = "일상"
+        })
+        let etc = UIAction(title: "기타", handler: { _ in
+            self.categoryButton.setTitle("기타", for: .normal)
+            self.category = "기타"
+        })
+        let menu = UIMenu(title: "", children: [study, project, life, etc])
+        categoryButton.setTitle("공부", for: .normal)
+        categoryButton.menu = menu
     }
     
     override func viewDidLoad() {
@@ -39,5 +65,6 @@ class AddViewController: UIViewController {
         datepicker.preferredDatePickerStyle = .wheels
         datepicker.addTarget(self, action: #selector(dateChange), for: .valueChanged)
         customLable2.inputView = datepicker
+        SetupCategoryMenu()
     }
 }
