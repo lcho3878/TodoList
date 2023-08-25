b5f7d80c72748d47563991c323515d8d60d174d6
chanho lee <leech3878@naver.com>
2023-08-25 17:10:22 +0900

    수정페이지 카테고리 수정 기능 추가

--- TodoList/ViewController/DetailViewController.swift
+++ TodoList/ViewController/DetailViewController.swift
@@ -12,13 +12,40 @@ class DetailViewController : UIViewController {
     var section: Int!
     var index : Int!
     var date : String!
+    var category: String?
+    @IBOutlet weak var categoryButton: UIButton!
     @IBOutlet weak var saveButton: UIButton!
     @IBOutlet var DetailView: UIView!
     @IBOutlet weak var customTextfield1: UITextField!
     @IBOutlet weak var customTextfield2: UITextField!
     
+    func SetupCategoryMenu () {
+        let study = UIAction(title: "공부", handler: { _ in
+            self.categoryButton.setTitle("공부", for: .normal)
+            self.category = "공부"
+        })
+        let project = UIAction(title: "프로젝트", handler: { _ in
+            self.categoryButton.setTitle("프로젝트", for: .normal)
+            self.category = "프로젝트"
+        })
+        let life = UIAction(title: "일상", handler: { _ in
+            self.categoryButton.setTitle("일상", for: .normal)
+            self.category = "일상"
+        })
+        let etc = UIAction(title: "기타", handler: { _ in
+            self.categoryButton.setTitle("기타", for: .normal)
+            self.category = "기타"
+        })
+        let menu = UIMenu(title: "", children: [study, project, life, etc])
+        categoryButton.setTitle(category, for: .normal)
+        categoryButton.menu = menu
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        category = todo[section][index].category
+        categoryButton.setTitle(category, for: .normal)
+        SetupCategoryMenu()
         customTextfield1.text = todo[section][index].content
         customTextfield2.text = todo[section][index].dueDate
         let datepicker = UIDatePicker()
@@ -36,8 +63,21 @@ class DetailViewController : UIViewController {
     @IBAction func Save(_ sender: Any) {
         let alert = UIAlertController(title: nil, message: "수정하시겠습니까?" , preferredStyle: .alert)
         let confirm = UIAlertAction(title: "확인", style: .default) { (ok) in
+            var newSection: Int
+            
+            switch self.category {
+            case "공부": newSection = 0
+            case "프로젝트": newSection = 1
+            case "일상": newSection = 2
+            default: newSection = 3
+            }
             todo[self.section][self.index].content = self.customTextfield1.text!
             todo[self.section][self.index].dueDate = self.customTextfield2.text!
+            if self.section != newSection {
+                let tempTodo = todo[self.section][self.index]
+                todo[newSection].append(tempTodo)
+                todo[self.section].remove(at: self.index)
+            }
             SaveData()
             self.navigationController?.popViewController(animated: true)
         }
